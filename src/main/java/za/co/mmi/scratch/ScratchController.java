193d457cef586bb7010320faf50837191d7ce4d3
shnaidoo1 <shivan.naidoo@metropolitan.co.za>
2018-03-03 20:24:37 +0200

    draft 1

--- src/main/java/za/co/mmi/scratch/ScratchController.java
+++ src/main/java/za/co/mmi/scratch/ScratchController.java
@@ -2,6 +2,8 @@ package za.co.mmi.scratch;
 
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -13,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -24,47 +26,48 @@ public class ScratchController {
 
     private static final Logger log = LoggerFactory.getLogger(ScratchController.class);
 
-    @RequestMapping(value="/getppt", method= RequestMethod.GET, consumes = { "application/json" })
+    @RequestMapping(value="/getppt", method= RequestMethod.POST)
     public ResponseEntity<byte[]> getPDF(@RequestBody String json) {
         log.debug(" -- received json payload " + json);
-        FileOutputStream fout = null;
+
+        ByteArrayOutputStream bout = null;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.presentationml.presentation"));
+        headers.setContentDispositionFormData("output.pptx", "output.pptx");
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
 
         try {
 
-            File file = new File("/tmp/tmp.pptx");
-            fout = new FileOutputStream(file);
-
+            bout = new ByteArrayOutputStream();
             XMLSlideShow ppt = new XMLSlideShow();
-            XSLFSlide blankSlide = ppt.createSlide();
 
-            ppt.write(fout);
-            fout.flush();
+            JSONArray jsonarray = new JSONArray(json);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                String title = jsonobject.getString("title");
+                log.debug(" -- title " + title);
+                String content = jsonobject.getString("content");
+                log.debug(" -- content:" + content);
+                XSLFSlide blankSlide = ppt.createSlide();
+            }
+
+            ppt.write(bout);
+            bout.flush();
+
+            return new ResponseEntity<>(bout.toByteArray(), headers, HttpStatus.OK);
 
         } catch (Exception e) {
             log.error(" -- error writing tmp ppt", e);
         } finally {
             try {
-                fout.close();
+                bout.close();
             } catch (IOException e) {
                 //squash
             }
         }
 
-//        PdfUtil.showHelp(emp);
-
-//        byte[] contents = (...);
-
-        byte[] contents = null;
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.presentationml.presentation"));
-        String filename = "output.pdf";
-        headers.setContentDispositionFormData(filename, filename);
-        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-
-        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
-
-        return response;
+        return new ResponseEntity<>(null, headers, HttpStatus.NO_CONTENT);
     }
 
 }
